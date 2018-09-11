eacccb9fd34254e86381a28d739e03a2a654ea15
drice82 <xujiabin82@gmail.com>
2018-09-11 11:23:23 +0800

    Update get.sh

--- clients/get.sh
+++ clients/get.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apt-get update
-apt-get install -y python python-pip gawk 
-wget 
+apt-get install -y python python-pip gawk vnstat
+pip install psutil
+wget https://github.com/drice82/ServerStatus-docker/raw/master/clients/client-psutil.py
